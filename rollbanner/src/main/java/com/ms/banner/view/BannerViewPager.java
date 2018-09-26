97a13c9af365bcce3cf0bcbbe474d8af6908f373
songwenchao <79556355@qq.com>
2018-09-26 10:00:37 +0800

    优化动画效果

--- rollbanner/src/main/java/com/ms/banner/view/BannerViewPager.java
+++ rollbanner/src/main/java/com/ms/banner/view/BannerViewPager.java
@@ -5,16 +5,21 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import java.lang.reflect.Field;
+
 public class BannerViewPager extends ViewPager {
 
     private boolean scrollable = true;
+    private boolean isShow;
 
     public BannerViewPager(Context context) {
         super(context);
+        isShow = true;
     }
 
     public BannerViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
+        isShow = true;
     }
 
     @Override
@@ -29,5 +34,24 @@ public class BannerViewPager extends ViewPager {
 
     public void setScrollable(boolean scrollable) {
         this.scrollable = scrollable;
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        try {
+            Field mFirstLayout = ViewPager.class.getDeclaredField("mFirstLayout");
+            mFirstLayout.setAccessible(true);
+            if (isShow) {
+                isShow = false;
+                mFirstLayout.set(this, true);
+            } else {
+                mFirstLayout.set(this, false);
+            }
+            getAdapter().notifyDataSetChanged();
+            setCurrentItem(getCurrentItem());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
