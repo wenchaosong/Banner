1168bf7b3419fbb65b123d326aedf63e74590bce
songwenchao <79556355@qq.com>
2018-10-12 10:13:43 +0800

    添加 indicator 在 banner 下面的 demo

--- app/src/main/java/com/test/demo/CustomViewPagerActivity.java
+++ app/src/main/java/com/test/demo/CustomViewPagerActivity.java
@@ -1,9 +1,13 @@
 package com.test.demo;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.ms.banner.Banner;
+import com.ms.banner.BannerConfig;
 import com.ms.banner.Transformer;
 import com.ms.banner.holder.BannerViewHolder;
 import com.ms.banner.holder.HolderCreator;
@@ -20,6 +24,11 @@ public class CustomViewPagerActivity extends AppCompatActivity {
     Banner banner1;
     Banner banner2;
     Banner banner3;
+    LinearLayout indicator;
+    private List<ImageView> indicatorImages = new ArrayList<>();
+    private int mIndicatorSelectedResId = R.mipmap.indicator;
+    private int mIndicatorUnselectedResId = R.mipmap.indicator2;
+    private int lastPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,8 @@ public class CustomViewPagerActivity extends AppCompatActivity {
         banner1 = (Banner) findViewById(R.id.banner1);
         banner2 = (Banner) findViewById(R.id.banner2);
         banner3 = (Banner) findViewById(R.id.banner3);
+        indicator = (LinearLayout) findViewById(R.id.indicator);
+        indicatorImages.clear();
 
         List<CustomData> list = new ArrayList<>();
         list.add(new CustomData("", "CustomLayout", false));
@@ -43,6 +54,8 @@ public class CustomViewPagerActivity extends AppCompatActivity {
         arrList.add(data2);
         arrList.add(data3);
 
+        initIndicator();
+
         banner1.setAutoPlay(true)
                 .setOffscreenPageLimit(list.size())
                 .setPages(list, new HolderCreator<BannerViewHolder>() {
@@ -51,8 +64,27 @@ public class CustomViewPagerActivity extends AppCompatActivity {
                         return new CustomViewHolder2();
                     }
                 })
+                .setBannerStyle(BannerConfig.NOT_INDICATOR)
                 .setBannerAnimation(Transformer.Scale)
                 .start();
+        banner1.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                indicatorImages.get((lastPosition + 3) % 3).setImageResource(mIndicatorUnselectedResId);
+                indicatorImages.get((position + 3) % 3).setImageResource(mIndicatorSelectedResId);
+                lastPosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         banner2.setAutoPlay(true)
                 .setOffscreenPageLimit(list.size())
@@ -73,6 +105,24 @@ public class CustomViewPagerActivity extends AppCompatActivity {
                     }
                 })
                 .start();
+    }
+
+    private void initIndicator() {
+        for (int i = 0; i < 3; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            LinearLayout.LayoutParams custom_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            custom_params.leftMargin = 2;
+            custom_params.rightMargin = 2;
+            if (i == 0) {
+                imageView.setImageResource(mIndicatorSelectedResId);
+            } else {
+                imageView.setImageResource(mIndicatorUnselectedResId);
+            }
+            indicatorImages.add(imageView);
+            indicator.addView(imageView, custom_params);
+        }
     }
 
     //如果你需要考虑更好的体验，可以这么操作
